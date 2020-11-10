c97be27ff2335388ba48cfd22388d765ad62a4c0
thamos-sp <74269077+thamos-sp@users.noreply.github.com>
2020-11-10 22:02:50 +0100

    Add files via upload

---
+++ public/install.js
@@ -0,0 +1,31 @@
+'use strict';
+
+let deferredInstallPrompt = null;
+const installButton = document.getElementById('install');
+installButton.addEventListener('click',installPWA);
+
+window.addEventListener('beforeinstallprompt',saveBeforeInstallPromptEvent);
+
+function saveBeforeInstallPromptEvent(evt) {
+    deferredInstallPrompt = evt;
+    installButton.removeAttribute("hidden");
+}
+
+function installPWA(evt) {
+    deferredInstallPrompt.prompt();
+    evt.srcElement.setAttribute('hidden',true);
+    deferredInstallPrompt.userChoice.then((choice)=>{
+        if (choice.outcome === "accepted") {
+            console.log("aceptado")
+        } else {
+            console.log("no aceptado")
+        }
+        deferredInstallPrompt = null;
+    })
+}
+
+window.addEventListener('appinstalled',logAppInstalled);
+
+function logAppInstalled(evt) {
+    console.log("APP instalada");
+}
